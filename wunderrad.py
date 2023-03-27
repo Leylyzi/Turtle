f06674c2f69ebfa70a87f85cdbd4db27738c5cce
Leyla Wieland <leyla@wieland.click>
2023-03-27 20:42:55 +0200

    init

---
+++ wunderrad.py
@@ -0,0 +1,14 @@
+function main() {
+	turtle->setSpeed(5000)
+	for(var i < 360) {
+		turtle->strichkreis(i)
+	}
+}
+turtle->learnWord->strichkreis(colour) {
+	turtle->penDown()
+	turtle->setPenColor(colour)
+	turtle->forward(100)
+	turtle->penUp()
+	turtle->back(100)
+	turtle->rightTurn(1)
+}
