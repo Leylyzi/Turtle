f93bfbd0bfb7b0c6290125a1571d3acd52c5a7c1
Leyla Wieland <leyla@wieland.click>
2023-03-27 20:41:41 +0200

    init

---
+++ hulahupring.py
@@ -0,0 +1,14 @@
+function main() {
+	turtle->setSpeed(5000)
+	for(var i < 360) {
+		turtle->strichkreis(i)
+	}
+}
+turtle->learnWord->strichkreis(colour) {
+	turtle->penDown()
+	turtle->setPenColor(colour)
+	turtle->forward(50)
+	turtle->penUp()
+	turtle->back(48)
+	turtle->rightTurn(1)
+}
